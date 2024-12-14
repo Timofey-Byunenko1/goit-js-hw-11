ea5e16cb0a0af62fa2a84a3a98b0c958b0424889
Timofey-Byunenko1 <byunenkotimofey@gmail.com>
2024-12-14 11:45:15 +0200

    add index file

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,5 +1,5 @@
 export async function fetchImages(query) {
-    const API_KEY = ':47637626-a9f1551754daf4452dc005acb';
+    const API_KEY = '47637626-a9f1551754daf4452dc005acb';
     const BASE_URL = 'https://pixabay.com/api/';
   
     const response = await fetch(
